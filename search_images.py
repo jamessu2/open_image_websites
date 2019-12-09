d5f5a225c5789a056b84387f0eccf035a6a87b9e
Jamie Su <jamessu2@users.noreply.github.com>
2019-12-08 20:38:27 -0500

    removed extra lines, main Python file now 50 lines total

--- search_images.py
+++ search_images.py
@@ -3,8 +3,7 @@ import os
 from selenium import webdriver
 
 # Dict of the sites to search. Adjust as desired.
-#	key = website URL
-#	value = HTML name of input box element
+#	key = website URL; value = HTML name of input box element
 sites_to_search = {
 	"https://unsplash.com/": "searchKeyword",
 	"https://www.pixabay.com": "q",
@@ -25,11 +24,9 @@ if __name__ == "__main__":
 			print(f"Can't open Chrome browser: '{err}'")
 		else:
 			print(f"***** Sites being searched for '{search_term}': ***** ")
-			
 			flag_opened = False
 			for site in sites_to_search:
 				print(f"- {site}")
-
 				try:
 					if not flag_opened:
 						# Open the 1st URL in a new browser window
@@ -39,7 +36,6 @@ if __name__ == "__main__":
 						# Open a new tab for next URL
 						browser.execute_script(f"window.open('{site}');")
 						browser.switch_to.window(browser.window_handles[-1])
-
 					# Access input box element on website, by name
 					python_button = browser.find_elements_by_xpath(f"//input[@name='{sites_to_search[site]}']")[0]
 					#	Note: You can also access the input box element by the full xpath. 
@@ -49,8 +45,6 @@ if __name__ == "__main__":
 					python_button.send_keys(search_term)
 					python_button.send_keys(u'\ue007')	# keycode for ENTER in selenium
 					print("\tSuccess!\n")
-
 				except Exception as err:
 					print(f"\tCan't search {site} properly.\n\tError: '{err}'", "\n")
-
 			print("***** Done searching. *****", "\n")
